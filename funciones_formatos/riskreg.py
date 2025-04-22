b82e7047a21679dbe53acec99e13c011a5bd4b19
Andrey <doto1816@gmail.com>
2025-04-21 20:21:39 -0500

    Se agrego el codigo para el auto completado de las id's al riskreg.py Ademas de probar la funcion

--- funciones_formatos/riskreg.py
+++ funciones_formatos/riskreg.py
@@ -4,13 +4,25 @@
 Created on Tue Apr  8 20:34:39 2025
 
 @author: javiermontanochiriboga
+@coauthor: yerdna14
 """
 
 import csv
+import os
 from datetime import datetime
 
 def riskreg():
     num_riskreg = int(input('¿Cuántos riesgos vas a registrar hoy? '))
+    # Revisar desde donde empezara a el nuevo id.
+    file_exists = os.path.isfile('issuelog.csv')
+    if file_exists:
+        with open('issuelog.csv', 'r', encoding='utf-8') as arch:
+            interx = csv.DictReader(arch)
+            ids_exis = [int(fil['ID']) for fil in interx if fil['ID'].isdigit()]
+            if ids_exis:
+                sig_id = max(ids_exis) + 1
+    else:
+        sig_id = 1
     
     with open('riskreg.csv', 'a', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
@@ -18,7 +30,7 @@ def riskreg():
         if csvfile.tell() == 0:
             writer.writerow(["ID", "Fecha de hallazgo", "WBS", "Rango", "Persona Asignada", "Descripción", "Impacto", "Probabilidad", "Estado"])
         
-        for count in range(1, num_riskreg + 1):
+        for count in range(sig_id, num_riskreg + sig_id):
             print(f'\n--- Riesgo #{count} ---')
             writer.writerow([
                 count,
